a2ad78dcde27595640c7ddc378b99fa697c479cd
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-03-02 00:27:06 +0600

    add more details

--- js/phone.js
+++ js/phone.js
@@ -3,6 +3,8 @@ const allPhones = () =>{
     const searchText = searchPhone.value;
     searchPhone.value = '';
     
+    
+    
     if(searchText == ''){
         document.body.style.display=alert('please enter Input field');
     }
@@ -18,17 +20,18 @@ const displayData = (phones) => {
     searchResult.textContent='';
     
     for (const phone of phones){
+        console.log(phone)
         const parent = document.getElementById('phones');
         const div = document.createElement('div');
         div.innerHTML=`
-            <div class="col">
-                <div class=" text-center card border">
+            <div class="col shadow-sm card-group">
+                <div class=" text-center card border p-2">
                     <div class=" pro-pic">
-                        <img  src="${phone.image}" alt="">
+                        <img src="${phone.image}" alt="">
                     </div>
                     <h5>Phone Name: ${phone.phone_name}</h5>
                     <p>Phone Brand:${phone.brand}</p>
-                    <div class="button">
+                    <div class="button ">
                         <button onclick="phoneDetails('${phone.slug}')" class="btn btn-success" >Details</button>
                     </div>
                 </div>
@@ -39,29 +42,39 @@ const displayData = (phones) => {
 };
 
 const phoneDetails = (id) => {
-   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-   fetch(url)
+    
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
    .then(res => res.json())
    .then(data => setDetails(data.data))
 };
 
 const setDetails = (info) => {
+    
     const phoneDetails = document.getElementById('phone-details');
-    const div = document.createElement('div');
+    phoneDetails.textContent='';
+    const div = document.createElement('div');    
     div.innerHTML=`
-    <div>
-        <div class="text-center">
-            <img class='w-25' src="${info.image}" alt="">
+    <div class="d-lg-flex shadow-sm mb-5 p-2">
+        <div class="my-auto">
+            <img src="${info.image}" alt="">
         </div>
         <div class="card-body">
-            <p>Display Size: ${info.mainFeatures.displaySize} </p>
-            <p>Chip: ${info.mainFeatures.chipSet}</p>
-            <p>Storages: ${info.mainFeatures.memory}</p>
-            <p>Phone ID: ${info.slug}</p>
-            <p>Release Date: ${info.releaseDate}</p>
+            <p><b>Display Size:</b> ${info.mainFeatures.displaySize} </p>
+            <p><b>Chip:</b> ${info.mainFeatures.chipSet}</p>
+            <p><b>Storages:</b> ${info.mainFeatures.memory}</p>
+            <p><b> Phone ID:</b> ${info.slug}</p>
+            <p><b>${info.releaseDate}</b></p>
+            <p><b>WLAN:</b> ${info.others ? info.others.WLAN:'not available'}</p>
+            <p><b>Bluetooth:</b> ${info.others ? info.others.Bluetooth:'not available'}</p>
+            <p><b>GPS:</b> ${info.others ? info.others.GPS:'not available'}</p>
+            <p><b>USB:</b> ${info.others ? info.others.USB:'not available'}</p>
         </div>
     </div>
     `
     phoneDetails.appendChild(div);
+    window.scrollTo(200,0)
 
 }
+
+
